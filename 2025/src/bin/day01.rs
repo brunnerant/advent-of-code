408a1c7e7e143ba5dd40fc2c1382d02ed6ee201c
Antoine Brunner <brunnerant@gmail.com>
2025-12-01 19:02:41 +0100

    More clever day 1 part 2

--- 2025/src/bin/day01.rs
+++ 2025/src/bin/day01.rs
@@ -1,47 +1,64 @@
 const INPUT: &str = include_str!("../../assets/day01.txt");
 
 fn parse(input: &str) -> Vec<i32> {
-    input.lines().map(|line| {
-        let mut line = line.chars();
-        let sign = line.next().expect("missing sign");
-        let sign = match sign {
-            'L' => -1,
-            'R' => 1,
-            _ => panic!("invalid sign"),
-        };
-        sign * line.as_str().parse::<i32>().expect("invalid number")
-    }).collect()
+    input
+        .lines()
+        .map(|line| {
+            let mut line = line.chars();
+            let sign = line.next().expect("missing sign");
+            let sign = match sign {
+                'L' => -1,
+                'R' => 1,
+                _ => panic!("invalid sign"),
+            };
+            sign * line.as_str().parse::<i32>().expect("invalid number")
+        })
+        .collect()
 }
 
 fn part1_algo(input: &str) -> usize {
     let input = parse(input);
     let mut lock = 50;
-    input.into_iter().filter(|&i| {
-        lock = (lock + i).rem_euclid(100);
-        lock == 0
-    }).count()
+    input
+        .into_iter()
+        .filter(|&i| {
+            lock = (lock + i).rem_euclid(100);
+            lock == 0
+        })
+        .count()
 }
 
 fn part1() -> usize {
     part1_algo(INPUT)
 }
 
-fn count_zeros(lock: &mut i32, offset: i32) -> usize {
+fn count_zeros(lock: i32, offset: i32) -> usize {
     // no movement -> nothing happens and we don't count going to zero again
     if offset == 0 {
         return 0;
     }
 
-    // question: how many multiples of 100 does the range [start, end] contain ?
-    let mut start = *lock + offset.signum();
-    let mut end = *lock + offset;
+    // question to ask: how many multiples of 100 does the range [start, end] contain ?
+    let mut start = lock + offset.signum();
+    let mut end = lock + offset;
+
+    // simplify the problem: always have an increasing interval start -> end
     if offset.signum() == -1 {
         start = -start;
         end = -end;
     }
-    
-    *lock = (*lock + offset).rem_euclid(100);
-    (start..=end).filter(|&i| i.rem_euclid(100) == 0).count()
+
+    // count how many full cycles are contained within the interval by the pigeonhole principle
+    let div = (end - start).div_euclid(100);
+    let result = div.abs() as usize;
+
+    // truncate the interval by that many full cycles and see if it contains one last occurence of zero
+    start += div * 100;
+    if start.div_euclid(100) != end.div_euclid(100) || start.rem_euclid(100) == 0 {
+        result + 1
+    } else {
+        result
+    }
 }
 
 fn part2_algo(input: &str) -> usize {
@@ -49,7 +66,8 @@ fn part2_algo(input: &str) -> usize {
     let mut lock = 50;
     let mut result: usize = 0;
     for i in input {
-        result += count_zeros(&mut lock, i);
+        result += count_zeros(lock, i);
+        lock = (lock + i).rem_euclid(100);
     }
     result
 }
@@ -68,7 +86,10 @@ mod tests {
     #[test]
     fn test_parse() {
         let input = "L68\nL30\nR48\nL5\nR60\nL55\nL1\nL99\nR14\nL82";
-        assert_eq!(super::parse(input), vec![-68, -30, 48, -5, 60, -55, -1, -99, 14, -82]);
+        assert_eq!(
+            super::parse(input),
+            vec![-68, -30, 48, -5, 60, -55, -1, -99, 14, -82]
+        );
     }
 
     #[test]
@@ -77,23 +98,20 @@ mod tests {
         assert_eq!(super::part1_algo(input), 3);
     }
 
-    fn assert_count_zero(mut lock: i32, offset: i32, expected: usize) {
-        assert_eq!(super::count_zeros(&mut lock, offset), expected)
-    }
-
     #[test]
     fn test_count_zeros() {
-        assert_count_zero(50, 50, 1);
-        assert_count_zero(50, -50, 1);
-        assert_count_zero(50, 60, 1);
-        assert_count_zero(50, -60, 1);
-        assert_count_zero(50, 160, 2);
-        assert_count_zero(50, -160, 2);
-        assert_count_zero(0, 100, 1);
-        assert_count_zero(0, -100, 1);
-        assert_count_zero(0, 200, 2);
-        assert_count_zero(0, 50, 0);
-        assert_count_zero(0, -50, 0);
+        assert_eq!(super::count_zeros(50, 50), 1);
+        assert_eq!(super::count_zeros(50, -50), 1);
+        assert_eq!(super::count_zeros(50, 60), 1);
+        assert_eq!(super::count_zeros(50, -60), 1);
+        assert_eq!(super::count_zeros(50, 160), 2);
+        assert_eq!(super::count_zeros(50, -160), 2);
+        assert_eq!(super::count_zeros(0, 100), 1);
+        assert_eq!(super::count_zeros(0, -100), 1);
+        assert_eq!(super::count_zeros(-1, 101), 2);
+        assert_eq!(super::count_zeros(0, 200), 2);
+        assert_eq!(super::count_zeros(0, 50), 0);
+        assert_eq!(super::count_zeros(0, -50), 0);
     }
 
     #[test]
