184ab4c6635bef5fa983e6db835b31b9165317c8
Antoine Brunner <brunnerant@gmail.com>
2025-12-01 22:15:43 +0100

    Update the template

--- 2025/template.rs
+++ 2025/template.rs
@@ -1,40 +1,44 @@
-
 const INPUT: &str = include_str!("../../assets/dayXY.txt");
 
-fn part1_algo(input: &str) -> usize {
-    todo!()
+type Input = ();
+
+fn parse(input: &str) -> Input {
+    unimplemented!()
 }
 
-fn part1() -> usize {
-    part1_algo(INPUT)
+fn part1(input: &Input) -> usize {
+    unimplemented!()
 }
 
-fn part2_algo(input: &str) -> usize {
-    todo!()
-}
-
-fn part2() -> usize {
-    part1_algo(INPUT)
+fn part2(input: &Input) -> usize {
+    unimplemented!()
 }
 
 pub fn main() {
-    println!("Day XY - Part 1: {}", part1());
-    println!("Day XY - Part 2: {}", part2());
+    let input = parse(INPUT);
+    println!("Day XY - Part 1: {}", part1(&input));
+    println!("Day XY - Part 2: {}", part2(&input));
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::dayXY::{part1_algo, part2_algo};
+    use crate::{parse, part1, part2};
+
+    #[test]
+    fn test_parse() {
+        let input = "";
+        assert_eq!(parse(input), ());
+    }
 
     #[test]
     fn test_part1() {
-        let input = "";
-        assert_eq!(part1_algo(input), 0);
+        let input = parse("");
+        assert_eq!(part1(&input), 42);
     }
 
     #[test]
     fn test_part2() {
-        let input = "";
-        assert_eq!(part2_algo(input), 0);
+        let input = parse("");
+        assert_eq!(part2(&input), 42);
     }
 }
