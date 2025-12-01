6a318329d67507d7a95c373c1af7170c4e2a8196
Antoine Brunner <brunnerant@gmail.com>
2025-12-01 18:42:00 +0100

    Day 1 part 2 stupid implementation

--- 2025/src/bin/day01.rs
+++ 2025/src/bin/day01.rs
@@ -26,17 +26,41 @@ fn part1() -> usize {
     part1_algo(INPUT)
 }
 
+fn count_zeros(lock: &mut i32, offset: i32) -> usize {
+    // no movement -> nothing happens and we don't count going to zero again
+    if offset == 0 {
+        return 0;
+    }
+
+    // question: how many multiples of 100 does the range [start, end] contain ?
+    let mut start = *lock + offset.signum();
+    let mut end = *lock + offset;
+    if offset.signum() == -1 {
+        start = -start;
+        end = -end;
+    }
+    
+    *lock = (*lock + offset).rem_euclid(100);
+    (start..=end).filter(|&i| i.rem_euclid(100) == 0).count()
+}
+
 fn part2_algo(input: &str) -> usize {
-    todo!()
+    let input = parse(input);
+    let mut lock = 50;
+    let mut result: usize = 0;
+    for i in input {
+        result += count_zeros(&mut lock, i);
+    }
+    result
 }
 
 fn part2() -> usize {
-    part1_algo(INPUT)
+    part2_algo(INPUT)
 }
 
 pub fn main() {
     println!("Day 01 - Part 1: {}", part1());
-    // println!("Day 01 - Part 2: {}", part2());
+    println!("Day 01 - Part 2: {}", part2());
 }
 
 #[cfg(test)]
@@ -51,5 +75,30 @@ mod tests {
     fn test_part1() {
         let input = "L68\nL30\nR48\nL5\nR60\nL55\nL1\nL99\nR14\nL82";
         assert_eq!(super::part1_algo(input), 3);
+    }
+
+    fn assert_count_zero(mut lock: i32, offset: i32, expected: usize) {
+        assert_eq!(super::count_zeros(&mut lock, offset), expected)
+    }
+
+    #[test]
+    fn test_count_zeros() {
+        assert_count_zero(50, 50, 1);
+        assert_count_zero(50, -50, 1);
+        assert_count_zero(50, 60, 1);
+        assert_count_zero(50, -60, 1);
+        assert_count_zero(50, 160, 2);
+        assert_count_zero(50, -160, 2);
+        assert_count_zero(0, 100, 1);
+        assert_count_zero(0, -100, 1);
+        assert_count_zero(0, 200, 2);
+        assert_count_zero(0, 50, 0);
+        assert_count_zero(0, -50, 0);
+    }
+
+    #[test]
+    fn test_part2() {
+        let input = "L68\nL30\nR48\nL5\nR60\nL55\nL1\nL99\nR14\nL82";
+        assert_eq!(super::part2_algo(input), 6);
     }
 }
