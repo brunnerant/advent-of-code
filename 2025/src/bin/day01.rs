e959a6b03ef0ee9fa492a222f224790de6995c3d
Antoine Brunner <brunnerant@gmail.com>
2025-12-02 22:55:56 +0100

    More compact day 1

--- 2025/src/bin/day01.rs
+++ 2025/src/bin/day01.rs
@@ -5,15 +5,7 @@ type Input = Vec<i32>;
 fn parse(input: &str) -> Input {
     input
         .lines()
-        .map(|line| {
-            let (dir, number) = line.split_at(1);
-            let sign = match dir {
-                "L" => -1,
-                "R" => 1,
-                _ => panic!("invalid sign"),
-            };
-            sign * number.parse::<i32>().expect("invalid number")
-        })
+        .map(|l| l.replace("L", "-").replace("R", "+").parse().unwrap())
         .collect()
 }
 
@@ -29,28 +21,11 @@ fn part1(input: &Input) -> usize {
 }
 
 fn count_zeros(lock: i32, offset: i32) -> usize {
-    // no movement -> nothing happens and we don't count going to zero again
-    if offset == 0 {
-        return 0;
-    }
-
-    // question to ask: how many multiples of 100 does the range [start, end] contain ?
-    let mut start = lock + offset.signum();
-    let mut end = lock + offset;
-
-    // simplify the problem: always have an increasing interval start -> end
-    if offset.signum() == -1 {
-        start = -start;
-        end = -end;
-    }
-
-    // count how many full cycles are contained within the interval by the pigeonhole principle
-    let div = (end - start).div_euclid(100);
-    let result = div.unsigned_abs() as usize;
-
-    // truncate the interval by that many full cycles and see if it contains one last occurence of zero
-    start += div * 100;
-    if start.div_euclid(100) != end.div_euclid(100) || start.rem_euclid(100) == 0 {
+    let result = (offset / 100).unsigned_abs() as usize;
+    let offset = offset % 100;
+    if lock == 0 {
+        result
+    } else if (offset > 0 && lock + offset >= 100) || (offset < 0 && lock + offset <= 0) {
         result + 1
     } else {
         result
@@ -102,7 +77,7 @@ mod tests {
         assert_eq!(count_zeros(50, -160), 2);
         assert_eq!(count_zeros(0, 100), 1);
         assert_eq!(count_zeros(0, -100), 1);
-        assert_eq!(count_zeros(-1, 101), 2);
+        assert_eq!(count_zeros(99, 101), 2);
         assert_eq!(count_zeros(0, 200), 2);
         assert_eq!(count_zeros(0, 50), 0);
         assert_eq!(count_zeros(0, -50), 0);
