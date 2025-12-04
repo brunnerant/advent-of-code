f0e7c14798ce9ca1679c8a5068f101a08b9396f9
Antoine Brunner <brunnerant@gmail.com>
2025-12-04 17:12:24 +0100

    Day 2 part 2

--- 2025/src/bin/day02.rs
+++ 2025/src/bin/day02.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 use itertools::Itertools;
 
 const INPUT: &str = include_str!("../../assets/day02.txt");
@@ -12,30 +14,43 @@ fn parse(input: &str) -> Input {
         .collect()
 }
 
+fn possible_numbers(a: usize, b: usize, num_groups: u32) -> impl Iterator<Item = usize> {
+    assert!(b >= a);
+    let a_digits = a.ilog10() + 1;
+    let b_digits = b.ilog10() + 1;
+    // Iterate over the possible number of digits in the repeated part
+    (a_digits.div_ceil(num_groups)..=(b_digits / num_groups)).flat_map(move |group_size| {
+        // Given the part p, the multiplier m is such that the full number n
+        // can be written as n = m * p
+        let base = 10usize.pow(group_size);
+        let multiplier = (0..num_groups).map(|i| base.pow(i)).sum();
+
+        // The part p must be comprised in range [base / 10, base - 1] because leading zeroes are not allowed
+        let min = a.div_ceil(multiplier).max(base / 10);
+        let max = (b / multiplier).min(base - 1);
+        (min..=max).map(move |x| x * multiplier)
+    })
+}
+
 fn part1(input: &Input) -> usize {
     let mut result = 0;
     for &(a, b) in input.iter() {
-        // The numbers need to have an even number of digits in the interval [a_digits, b_digits]
-        let a_digits = a.ilog10() + 1;
-        let b_digits = b.ilog10() + 1;
-        for d in a_digits.div_ceil(2)..=(b_digits / 2) {
-            // Each half has d digits, so it needs to lie in the interval [10^(d - 1), 10^d - 1]
-            // Furthermore, if h denotes the half, we need to have a <= h * 10^d + h = h * (10^d + 1) <= b.
-            // Or equivalently, a / (10^d + 1) <= h <= b / (10^d + 1).
-            // We just need to be careful about rounding in the right diretcion.
-            let base = 10usize.pow(d);
-            let min = a.div_ceil(base + 1).max(base / 10);
-            let max = (b / (base + 1)).min(base - 1);
-            for x in min..=max {
-                result += (base + 1) * x;
-            }
-        }
+        result += possible_numbers(a, b, 2).sum::<usize>();
     }
     result
 }
 
 fn part2(input: &Input) -> usize {
-    unimplemented!()
+    let mut result = 0;
+    let mut distinct = HashSet::new();
+    for &(a, b) in input.iter() {
+        let b_digits = b.ilog10() + 1;
+        for num_groups in 2..=b_digits {
+            distinct.extend(possible_numbers(a, b, num_groups));
+        }
+        result += distinct.drain().sum::<usize>();
+    }
+    result
 }
 
 pub fn main() {
@@ -58,9 +73,13 @@ mod tests {
         assert_eq!(part1(&input), 1227775554);
     }
 
-    // #[test]
-    // fn test_part2() {
-    //     let input = parse("");
-    //     assert_eq!(part2(&input), 42);
-    // }
+    #[test]
+    fn test_part2() {
+        let input = parse(
+            "11-22,95-115,998-1012,1188511880-1188511890,\
+            222220-222224,1698522-1698528,446443-446449,38593856-38593862,\
+            565653-565659,824824821-824824827,2121212118-2121212124",
+        );
+        assert_eq!(part2(&input), 4174379265);
+    }
 }
