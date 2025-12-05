45f358c3e4aab3774024ee309ba88c92086f9eae
Antoine Brunner <brunnerant@gmail.com>
2025-12-05 14:08:29 +0100

    Day 4

---
+++ 2025/src/bin/day04.rs
@@ -0,0 +1,108 @@
+use std::collections::HashSet;
+
+use aoc2025::grid::Grid;
+use itertools::Itertools;
+
+const INPUT: &str = include_str!("../../assets/day04.txt");
+
+type Input = Grid<bool>;
+
+fn parse(input: &str) -> Input {
+    Grid::from_lines(input.lines().map(|l| {
+        l.chars().map(|c| match c {
+            '@' => true,
+            '.' => false,
+            _ => panic!("invalid grid character"),
+        })
+    }))
+    .unwrap()
+}
+
+fn part1(input: &Input) -> usize {
+    let mut result = 0;
+    for y in 0..input.height {
+        for x in 0..input.width {
+            if !input[(x, y)] {
+                continue;
+            }
+            let num_adjacent = input
+                .adjacent_cells((x, y))
+                .filter(|&(i, j)| input[(i, j)])
+                .count();
+            if num_adjacent < 4 {
+                result += 1;
+            }
+        }
+    }
+    result
+}
+
+fn part2(input: &Input) -> usize {
+    let mut input = input.clone();
+    let mut result = 0;
+    let mut to_check =
+        HashSet::<(usize, usize)>::from_iter((0..input.width).cartesian_product(0..input.height));
+    while !to_check.is_empty() {
+        let &(x, y) = to_check.iter().next().unwrap();
+        to_check.remove(&(x, y));
+
+        if !input[(x, y)] {
+            continue;
+        }
+        let num_adjacent = input
+            .adjacent_cells((x, y))
+            .filter(|&(i, j)| input[(i, j)])
+            .count();
+        if num_adjacent < 4 {
+            result += 1;
+            to_check.extend(input.adjacent_cells((x, y)));
+            input[(x, y)] = false;
+        }
+    }
+    result
+}
+
+pub fn main() {
+    let input = parse(INPUT);
+    println!("Day 04 - Part 1: {}", part1(&input));
+    println!("Day 04 - Part 2: {}", part2(&input));
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{parse, part1, part2};
+
+    #[test]
+    fn test_part1() {
+        let input = parse(
+            "..@@.@@@@.\n\
+             @@@.@.@.@@\n\
+             @@@@@.@.@@\n\
+             @.@@@@..@.\n\
+             @@.@@@@.@@\n\
+             .@@@@@@@.@\n\
+             .@.@.@.@@@\n\
+             @.@@@.@@@@\n\
+             .@@@@@@@@.\n\
+             @.@.@@@.@.",
+        );
+        assert_eq!(part1(&input), 13);
+    }
+
+    #[test]
+    fn test_part2() {
+        let input = parse(
+            "..@@.@@@@.\n\
+             @@@.@.@.@@\n\
+             @@@@@.@.@@\n\
+             @.@@@@..@.\n\
+             @@.@@@@.@@\n\
+             .@@@@@@@.@\n\
+             .@.@.@.@@@\n\
+             @.@@@.@@@@\n\
+             .@@@@@@@@.\n\
+             @.@.@@@.@.",
+        );
+        assert_eq!(part2(&input), 43);
+    }
+}
---
+++ 2025/src/grid.rs
@@ -0,0 +1,61 @@
+use std::ops::{Index, IndexMut};
+
+use itertools::Itertools;
+
+#[derive(Debug, Clone)]
+pub struct Grid<T> {
+    cells: Vec<T>,
+    pub width: usize,
+    pub height: usize,
+}
+
+impl<T> Grid<T> {
+    pub fn from_lines(lines: impl Iterator<Item = impl Iterator<Item = T>>) -> Result<Self, ()> {
+        let mut cells = Vec::new();
+        let mut width = None;
+        let mut height = 0;
+        for line in lines {
+            let size_before = cells.len();
+            cells.extend(line);
+            let line_size = cells.len() - size_before;
+            if let Some(w) = width {
+                if w != line_size {
+                    return Err(());
+                }
+            } else {
+                width = Some(line_size);
+            }
+            height += 1;
+        }
+        Ok(Grid {
+            cells,
+            width: width.unwrap_or(0),
+            height,
+        })
+    }
+
+    pub fn adjacent_cells(&self, (x, y): (usize, usize)) -> impl Iterator<Item = (usize, usize)> {
+        let x0: isize = if x > 0 { -1 } else { 0 };
+        let x1: isize = if x < self.width - 1 { 1 } else { 0 };
+        let y0: isize = if y > 0 { -1 } else { 0 };
+        let y1: isize = if y < self.height - 1 { 1 } else { 0 };
+        (x0..=x1)
+            .cartesian_product(y0..=y1)
+            .filter(|&(i, j)| (i, j) != (0, 0))
+            .map(move |(i, j)| (x.wrapping_add_signed(i), y.wrapping_add_signed(j)))
+    }
+}
+
+impl<T> Index<(usize, usize)> for Grid<T> {
+    type Output = T;
+
+    fn index(&self, (x, y): (usize, usize)) -> &Self::Output {
+        &self.cells[y * self.width + x]
+    }
+}
+
+impl<T> IndexMut<(usize, usize)> for Grid<T> {
+    fn index_mut(&mut self, (x, y): (usize, usize)) -> &mut Self::Output {
+        &mut self.cells[y * self.width + x]
+    }
+}
